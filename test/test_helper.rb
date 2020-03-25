f8e1555cdfff258f0dc33d52da3f4ae11bf417be
Eric Larson <ewlarson@gmail.com>
2020-03-25 14:56:45 -0500

    Tests: add devise, first controller test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,20 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
+  include Devise::Test::IntegrationHelpers
+  include Warden::Test::Helpers
+
+  def log_in(user)
+    if integration_test?
+      # use warden helper
+      login_as(user, scope: user)
+    else # controller_test, model_test
+      # user devise helper
+      sign_in(user)
+    end
+  end
 
   # Add more helper methods to be used by all tests here...
 end
