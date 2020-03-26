8e45bc2541623ba0c0ef6a7bc239b1cdc87ca62b
Eric Larson <ewlarson@gmail.com>
2020-03-26 13:51:38 -0500

    Tests: continued document and document_indexer

--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -9,7 +9,21 @@ class DocumentTest < ActiveSupport::TestCase
     assert @document.respond_to? :title
   end
 
+  test 'responds to type' do
+    assert @document.respond_to? :type
+    assert_equal @document.type, 'Document'
+  end
+
   test 'responds to json_attributes' do
     assert @document.respond_to? :json_attributes
+  end
+
+  test 'responds to timestamps' do
+    assert @document.respond_to? :created_at
+    assert @document.respond_to? :updated_at
+  end
+
+  test 'responds to friendlier_id' do
+    assert @document.respond_to? :friendlier_id
   end
 end
