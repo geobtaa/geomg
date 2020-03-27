2624a4cef7006f8c46d0ea270cf808b18e44958a
Eric Larson <ewlarson@gmail.com>
2020-03-27 17:00:23 -0500

    Update document_form_test.rb

--- test/system/document_form_test.rb
+++ test/system/document_form_test.rb
@@ -15,4 +15,18 @@ class DocumentFormTest < ApplicationSystemTestCase
     assert page.has_selector?("#form-fields")       # Form Fields
     assert page.has_selector?("#form-navigation")   # Form Navigation
   end
+
+  def test_form_elements
+    visit "/documents/new"
+    within('form#new_document') do
+      #dc_title_s
+      assert page.has_selector?("input#document_title")
+      #dct_alternativeTitle_sm
+      assert page.has_selector?("input#document_dct_alternativeTitle_sm_attributes_", visible: false)
+      #dc_description_s
+      assert page.has_selector?("textarea#document_dc_description_s")
+      #dc_language_sm
+      assert page.has_selector?("input#document_dc_language_sm_attributes_", visible: false)
+    end
+  end
 end
