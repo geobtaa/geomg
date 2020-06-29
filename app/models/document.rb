1761feebe532b260abe78b2f5bd5d0e5ab570a3a
Eric Larson <ewlarson@gmail.com>
2020-06-29 14:38:20 -0500

    Document: determine solr_year for indexing

--- app/models/document.rb
+++ app/models/document.rb
@@ -106,8 +106,10 @@ class Document < Kithe::Work
 
   def solr_year_json
     solr_year = nil
-    start_d, _end_d = b1g_date_range_drsim.first.split('-')
-    solr_year = start_d if start_d.present?
+    return unless b1g_date_range_drsim.present?
+      start_d, _end_d = b1g_date_range_drsim.first.split('-')
+      solr_year = start_d if start_d.present?
+    end
   end
 
   # Export Transformations - to_*
