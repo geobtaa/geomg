73bd2dd2534a0529121cfa4000ae00d4252062ec
Michael Berkowski <mjb@umn.edu>
2020-07-20 11:22:52 -0500

    docker-comopse env vars

--- docker/postgresql/geomg-pg-init.sh
+++ docker/postgresql/geomg-pg-init.sh
@@ -6,9 +6,7 @@ set -u
 
 # Inspired by https://github.com/mrts/docker-postgresql-multiple-databases/blob/master/create-multiple-postgresql-databases.sh
 # We only need one user
-if [ -n $POSTGRES_MULTIPLE_DATABASES ]; then
-  for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr , ' '); do
-    # Create each database comma-separated, grant full rights to the pg user
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE $db" -c "GRANT ALL PRIVILEGES ON DATABASE $db TO $POSTGRES_USER;"
-  done
-fi
+for db in geomg_test geomg_development; do
+  # Create each database comma-separated, grant full rights to the pg user
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE $db" -c "GRANT ALL PRIVILEGES ON DATABASE $db TO $POSTGRES_USER;"
+done
