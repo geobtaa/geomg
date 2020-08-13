3c96466e0863386d151a0b18515063d93333dc74
Eric Larson <ewlarson@gmail.com>
2020-08-13 16:35:15 -0500

    StimulusReflex: remove feature, no hosting support for ActionCable yet
    Can revisit when Apache+Passenger is revisited.

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,16 +3,7 @@
 module ApplicationCable
   # Connect!
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
-
     def connect
-      user_id = cookies.encrypted[:user_id]
-      return reject_unauthorized_connection if user_id.nil?
-
-      user = User.find_by(id: user_id)
-      return reject_unauthorized_connection if user.nil?
-
-      self.current_user = user
     end
   end
 end
