014369ae91112d097a7406cdc491fcdf5e86937b
Eric Larson <ewlarson@gmail.com>
2020-11-02 11:19:33 -0600

    Gems: add letter_opener for local development
    Emails generated by the app will appear in a new browser tab now.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'geomg.test' }
 
   config.action_mailer.perform_caching = false
 
