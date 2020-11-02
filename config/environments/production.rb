617ca0ed870b06563f9d87acac71359812db2e4f
Eric Larson <ewlarson@gmail.com>
2020-11-02 11:36:44 -0600

    Email: update production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,11 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "geomg_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: %{"B1G GEOMG" <swadm@#{`hostname`.strip}>}}
+  config.action_mailer.default_url_options = { :host => `hostname`.strip }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
