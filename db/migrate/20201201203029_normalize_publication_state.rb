78c2b3bc937a143df4ec0ee1703cb88dd0095608
Eric Larson <ewlarson@gmail.com>
2020-12-03 16:39:20 -0600

    Update 20201201203029_normalize_publication_state.rb
    Use up and down

--- db/migrate/20201201203029_normalize_publication_state.rb
+++ db/migrate/20201201203029_normalize_publication_state.rb
@@ -1,8 +1,12 @@
 class NormalizePublicationState < ActiveRecord::Migration[6.0]
-  def change
+  def up
     execute "UPDATE kithe_models SET publication_state = LOWER(publication_state)"
     execute "UPDATE document_transitions SET to_state = LOWER(to_state)"
 
     Rake::Task['geomg:solr:reindex'].invoke
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
