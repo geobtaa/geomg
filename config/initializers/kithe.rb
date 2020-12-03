30f5b71a11b40d7f39c05cecffa146e3effd7710
Eric Larson <ewlarson@gmail.com>
2020-12-03 09:16:30 -0600

    Kithe: continue processing despite errors; custom logger
    Setting max_skipped to -1 will ensure indexing process continues despite errors. Logging indexing to log/traject.log

--- config/initializers/kithe.rb
+++ config/initializers/kithe.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+traject_logger = ActiveSupport::Logger.new("#{Rails.root}/log/traject.log")
+
 Kithe.indexable_settings.solr_url = ENV['SOLR_URL']
 
 # Index to solr with solr `id` field being our friendlier_id
@@ -8,8 +10,10 @@ Kithe.indexable_settings.solr_id_value_attribute = :friendlier_id
 # Traject settings
 Kithe.indexable_settings.writer_settings = {
   'solr_writer.thread_pool' => 0,
-  'solr_writer.batch_size' => 1, 'solr_writer.solr_update_args' => { softCommit: true }, 'solr_writer.http_timeout' => 3,
-  'logger' => nil
+  'solr_writer.batch_size' => 1,
+  'solr_writer.solr_update_args' => { softCommit: true }, 'solr_writer.http_timeout' => 3,
+  'solr_writer.max_skipped' => -1,
+  'logger' => traject_logger
 }
 
 # Traject with Basic Auth support
