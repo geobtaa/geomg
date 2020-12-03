56960951442cad519a24f3c44ec863545e4b6397
Eric Larson <ewlarson@gmail.com>
2020-12-03 16:31:25 -0600

    Update 20201201203029_normalize_publication_state.rb
    Raw sql to update the column values to lowercase. Reindex whole database.

--- db/migrate/20201201203029_normalize_publication_state.rb
+++ db/migrate/20201201203029_normalize_publication_state.rb
@@ -1,47 +1,8 @@
 class NormalizePublicationState < ActiveRecord::Migration[6.0]
   def change
-    errors = []
+    execute "UPDATE kithe_models SET publication_state = LOWER(publication_state)"
+    execute "UPDATE document_transitions SET to_state = LOWER(to_state)"
 
-    docs = DocumentTransition.where(to_state: "Draft")
-    docs.each do |doc|
-      doc.to_state = 'draft'
-      begin
-        doc.save
-      rescue
-        errors << "Failed Save - Doc - #{doc.friendlier_id}"
-      end
-    end
-
-    docs = DocumentTransition.where(to_state: "Published")
-    docs.each do |doc|
-      doc.to_state = 'published'
-      begin
-        doc.save
-      rescue
-        errors << "Failed Save - Doc - #{doc.friendlier_id}"
-      end
-    end
-
-    docs = DocumentTransition.where(to_state: "Unpublished")
-    docs.each do |doc|
-      doc.to_state = 'unpublished'
-      begin
-        doc.save
-      rescue
-        errors << "Failed Save - Doc - #{doc.friendlier_id}"
-      end
-    end
-
-    docs = Document.all
-    docs.each do |doc|
-      # Should update Solr with new publication state
-      begin
-        doc.save
-      rescue
-        errors << "Failed Save - Doc - #{doc.friendlier_id}"
-      end
-    end
-
-    puts errors.inspect
+    Rake::Task['geomg:solr:reindex'].invoke
   end
 end
