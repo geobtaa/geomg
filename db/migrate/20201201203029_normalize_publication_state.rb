8b90fee34eed041232f97740c3540cef5d1ef651
Eric Larson <ewlarson@gmail.com>
2020-12-04 11:05:52 -0600

    DB Migration: remove reindex call. Do that manually.

--- db/migrate/20201201203029_normalize_publication_state.rb
+++ db/migrate/20201201203029_normalize_publication_state.rb
@@ -2,8 +2,6 @@ class NormalizePublicationState < ActiveRecord::Migration[6.0]
   def up
     execute "UPDATE kithe_models SET publication_state = LOWER(publication_state)"
     execute "UPDATE document_transitions SET to_state = LOWER(to_state)"
-
-    Rake::Task['geomg:solr:reindex'].invoke
   end
 
   def down
