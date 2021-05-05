a918cfb27c3b133e76435fae67829ee751390b57
Eric Larson <ewlarson@gmail.com>
2021-05-05 08:25:54 -0500

    Tests: add search results and advanced search tests

---
+++ test/system/search_advanced_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class SearchAdvancedTest < ApplicationSystemTestCase
+  def setup
+    sign_in_as users(:user_001)
+  end
+
+  test 'search advanced dom' do
+    visit '/search'
+    assert page.has_selector?('nav.navbar')
+    assert page.has_selector?('#search-form')
+    within('#search-form') do
+      assert page.has_selector?("input[name='q']")
+      assert page.has_selector?('#facet-limits')
+      within('#facet-limits') do
+        assert page.has_text?('Resource Class')
+        assert page.has_text?('Provider')
+        assert page.has_text?('Code')
+        assert page.has_text?('Is Part Of')
+        assert page.has_text?('Member Of')
+        assert page.has_text?('Resource Type')
+        assert page.has_text?('Subject')
+        assert page.has_text?('ISO Topic Category')
+        assert page.has_text?('Format')
+        assert page.has_text?('Suppressed')
+        assert page.has_text?('Child Record')
+        assert page.has_text?('Georeferenced')
+      end
+    end
+  end
+end
