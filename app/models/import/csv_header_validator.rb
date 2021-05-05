ee7e68a0df6bcb573f1b409bf185d5a68f5b93c0
Eric Larson <ewlarson@gmail.com>
2021-05-05 17:33:32 -0500

    Update csv_header_validator.rb
    Test validity from parsed file download headers

--- app/models/import/csv_header_validator.rb
+++ app/models/import/csv_header_validator.rb
@@ -8,7 +8,7 @@ class Import
   class CsvHeaderValidator < ActiveModel::Validator
     def validate(record)
       valid_csv_header = true
-      unless valid_csv_headers?(record.headers)
+      unless valid_csv_headers?(record.csv_file)
         valid_csv_header = false
         record.errors.add(:csv_file, 'Missing a required CSV header. Title, Resource Class, Access Rights, and ID are required.')
       end
@@ -16,7 +16,8 @@ class Import
       valid_csv_header
     end
 
-    def valid_csv_headers?(headers)
+    def valid_csv_headers?(csv_file)
+      headers = CSV.parse(csv_file.download)[0]
       if (['Title', 'Resource Class', 'Access Rights', 'ID'] - headers).empty?
         true
       else
