8d5df3dc8bd5a709e2a1bee07943477ca8005b6a
Eric Larson <ewlarson@gmail.com>
2022-07-15 17:06:08 -0500

    Geom Migration
    Populate the geometry field as a POLYGON via a bbox value.

---
+++ db/migrate/20220715205346_populate_geometry.rb
@@ -0,0 +1,22 @@
+class PopulateGeometry < ActiveRecord::Migration[6.1]
+  def change
+    @steps = 0
+    Document.in_batches(of: 1000).each do |relation|
+      relation.each do |doc|
+        begin
+          if doc.dcat_bbox.present?
+            doc.locn_geometry = doc.derive_polygon
+          else
+            doc.locn_geometry = ''
+          end
+          doc.save
+        rescue Exception => e
+          puts "Failed: #{doc.id} - #{e.inspectx}"
+        end
+      end
+
+      @steps = @steps + relation.count
+      puts "Docs Migrated: #{@steps}"
+    end
+  end
+end
