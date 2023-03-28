6357d271b7aeffab084754ea4f86013a66e07587
Eric Larson <ewlarson@gmail.com>
2023-03-28 16:02:20 -0500

    Shrine: test re-config

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -2,8 +2,8 @@ require "shrine"
 require "shrine/storage/file_system"
 
 Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads")       # permanent
+  cache: Shrine::Storage::FileSystem.new(Rails.root.join("public"), prefix: "uploads/cache"),
+  store: Shrine::Storage::FileSystem.new(Rails.root.join("public"), prefix: "uploads/store")       # permanent
 }
 
 Shrine.plugin :activerecord
